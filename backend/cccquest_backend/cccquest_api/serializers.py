859e8baf8569fbb10e2c21d444c1ee86d6f14d71
Johannes Burk <me@jojoob.de>
2021-01-25 19:25:13 +0100

    Add basic RESTful API implementation for defined models.

---
+++ backend/cccquest_backend/cccquest_api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Answer, Choice, Iteration, Question
+
+class ChoiceSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Choice
+		fields = ('id', 'question', 'choice_text')
+
+class QuestionSerializer(serializers.ModelSerializer):
+	choices = ChoiceSerializer(many=True, read_only=True)
+
+	class Meta:
+		model = Question
+		fields = ('id', 'question_text', 'choices')
+
+class IterationSerializer(serializers.ModelSerializer):
+	answers = serializers.StringRelatedField(many=True)
+
+	class Meta:
+		model = Iteration
+		fields = ('id', 'iteration_title', 'created', 'complete', 'answers')
--- backend/cccquest_backend/cccquest_api/views.py
+++ backend/cccquest_backend/cccquest_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import QuestionSerializer, ChoiceSerializer, IterationSerializer
+from .models import Question, Choice, Iteration
 
-# Create your views here.
+class QuestionView(viewsets.ModelViewSet):
+	serializer_class = QuestionSerializer
+	queryset = Question.objects.all()
+
+class ChoiceView(viewsets.ModelViewSet):
+	serializer_class = ChoiceSerializer
+	queryset = Choice.objects.all()
+
+class IterationView(viewsets.ModelViewSet):
+	serializer_class = IterationSerializer
+	queryset = Iteration.objects.all()
