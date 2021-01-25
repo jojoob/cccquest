1cc1a0800ce01e7ce206b507544295c370de20fd
Johannes Burk <me@jojoob.de>
2021-01-25 16:39:38 +0100

    Create django app cccquest_api

--- backend/cccquest_backend/cccquest_backend/settings.py
+++ backend/cccquest_backend/cccquest_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cccquest_api.apps.CccquestApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
