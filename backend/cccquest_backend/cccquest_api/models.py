098401b52d1834e1ccacaba28a4de9302f6f2414
Johannes Burk <me@jojoob.de>
2021-01-25 16:57:27 +0100

    Add models

--- backend/cccquest_backend/cccquest_api/models.py
+++ backend/cccquest_backend/cccquest_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.question_text
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+
+class Iteration(models.Model):
+	iteration_title = models.CharField(max_length=200)
+	created = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField()
+
+class Answer(models.Model):
+	iteration = models.ForeignKey(Iteration, on_delete=models.CASCADE)
+	choice = models.ForeignKey(Choice, on_delete=models.PROTECT)
